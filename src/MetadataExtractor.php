8ced1cbd818db4d6fc5a4adc07c32eabf5608f7e
Carlos C Soto <eclipxe13@gmail.com>
2019-04-04 17:32:26 -0600

    change array construction on obtainMetadataValues

--- src/MetadataExtractor.php
+++ src/MetadataExtractor.php
@@ -41,22 +41,23 @@ class MetadataExtractor
     public function obtainMetadataValues(Crawler $row): array
     {
         $tds = $row->filter('td[style="WORD-BREAK:BREAK-ALL;"]');
-
-        $temp['uuid'] = trim($tds->getNode(0)->textContent);
-        $temp['rfcEmisor'] = trim($tds->getNode(1)->textContent);
-        $temp['nombreEmisor'] = trim($tds->getNode(2)->textContent);
-        $temp['rfcReceptor'] = trim($tds->getNode(3)->textContent);
-        $temp['nombreReceptor'] = trim($tds->getNode(4)->textContent);
-        $temp['fechaEmision'] = trim($tds->getNode(5)->textContent);
-        $temp['fechaCertificacion'] = trim($tds->getNode(6)->textContent);
-        $temp['pacCertifico'] = trim($tds->getNode(7)->textContent);
-        $temp['total'] = trim($tds->getNode(8)->textContent);
-        $temp['efectoComprobante'] = trim($tds->getNode(9)->textContent);
-        $temp['estatusCancelacion'] = trim($tds->getNode(10)->textContent);
-        $temp['estadoComprobante'] = trim($tds->getNode(11)->textContent);
-        $temp['estatusProcesoCancelacion'] = trim($tds->getNode(12)->textContent);
-        $temp['fechaProcesoCancelacion'] = trim($tds->getNode(13)->textContent);
-        return $temp;
+        
+        return [
+            'uuid' => trim($tds->getNode(0)->textContent),
+            'rfcEmisor' => trim($tds->getNode(1)->textContent),
+            'nombreEmisor' => trim($tds->getNode(2)->textContent),
+            'rfcReceptor' => trim($tds->getNode(3)->textContent),
+            'nombreReceptor' => trim($tds->getNode(4)->textContent),
+            'fechaEmision' => trim($tds->getNode(5)->textContent),
+            'fechaCertificacion' => trim($tds->getNode(6)->textContent),
+            'pacCertifico' => trim($tds->getNode(7)->textContent),
+            'total' => trim($tds->getNode(8)->textContent),
+            'efectoComprobante' => trim($tds->getNode(9)->textContent),
+            'estatusCancelacion' => trim($tds->getNode(10)->textContent),
+            'estadoComprobante' => trim($tds->getNode(11)->textContent),
+            'estatusProcesoCancelacion' => trim($tds->getNode(12)->textContent),
+            'fechaProcesoCancelacion' => trim($tds->getNode(13)->textContent),
+        ];
     }
 
     public function obtainUrlXml(Crawler $row): ?string
