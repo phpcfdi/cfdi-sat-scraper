1eacc12e4994224776d42020a294bf0190593a82
Carlos C Soto <eclipxe13@gmail.com>
2019-04-26 11:14:05 -0500

    Remove unused class Helpers
    converterSecondsToHours was not required anymore, use directly gmdate
    formatNumber was not used in the code

--- src/SATScraper.php
+++ src/SATScraper.php
@@ -447,7 +447,7 @@ class SATScraper
 
     protected function buildDateWithDayAndSeconds(\DateTimeImmutable $day, int $seconds): \DateTimeImmutable
     {
-        $timeAsString = Helpers::converterSecondsToHours($seconds);
+        $timeAsString = gmdate('H:i:s', $seconds);
         [$hour, $minute, $second] = explode(':', $timeAsString);
         $date = $day->setTime((int) $hour, (int) $minute, (int) $second);
         return $date;
