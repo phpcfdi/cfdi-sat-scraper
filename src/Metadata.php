ea4078509c8ea6d18c929f21a2085b191df49e91
Carlos C Soto <eclipxe13@gmail.com>
2019-04-30 18:21:54 -0500

    Use array instead of stdClass for internal collection of properties

--- src/Metadata.php
+++ src/Metadata.php
@@ -4,32 +4,29 @@ declare(strict_types=1);
 
 namespace PhpCfdi\CfdiSatScraper;
 
-use stdClass;
-
 class Metadata
 {
-    /** @var stdClass */
+    /** @var array */
     private $data;
 
     public function __construct(string $uuid, array $data = [])
     {
-        $data = (object) $data;
         $this->data = $data;
-        $this->data->uuid = $uuid;
+        $this->data['uuid'] = $uuid;
     }
 
     public function uuid(): string
     {
-        return $this->data->uuid;
+        return $this->data['uuid'];
     }
 
     public function get(string $key): string
     {
-        return strval($this->data->{$key} ?? '');
+        return strval($this->data[$key] ?? '');
     }
 
     public function has(string $key): bool
     {
-        return isset($this->data->{$key});
+        return isset($this->data[$key]);
     }
 }
