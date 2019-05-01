68d4d05755791371eb5b5c603d65d7a554767ca3
Carlos C Soto <eclipxe13@gmail.com>
2019-04-30 19:12:59 -0500

    Do not allow empty string as UUID

--- src/Metadata.php
+++ src/Metadata.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace PhpCfdi\CfdiSatScraper;
 
+use InvalidArgumentException;
+
 class Metadata
 {
     /** @var array */
@@ -11,6 +13,9 @@ class Metadata
 
     public function __construct(string $uuid, array $data = [])
     {
+        if ('' === $uuid) {
+            throw new InvalidArgumentException('UUID cannot be empty');
+        }
         $this->data = ['uuid' => $uuid] + $data;
     }
 
