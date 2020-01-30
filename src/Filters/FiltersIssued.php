a6a936ba54e1cdee852e69600fa9b829398f6247
Carlos C Soto <eclipxe13@gmail.com>
2020-01-30 17:52:59 -0600

    Fix FiltersIssued
    It was sending with incorrect format (zeros):
    - dates, hours, minutes and seconds
    It was using start date for date and year of end date, it is not a problem wight now because every query is stored in just one day
    Includes new fields

--- src/Filters/FiltersIssued.php
+++ src/Filters/FiltersIssued.php
@@ -16,24 +16,22 @@ class FiltersIssued extends BaseFilters implements Filters
      */
     public function getFilters(): array
     {
-        $startYear = $this->query->getStartDate()->format('Y');
-        $startMonth = $this->query->getStartDate()->format('n');
-        $startDay = $this->query->getStartDate()->format('d');
-
         return [
             '__ASYNCPOST' => 'true',
             '__EVENTARGUMENT' => '',
             '__EVENTTARGET' => '',
             '__LASTFOCUS' => '',
             'ctl00$MainContent$BtnBusqueda' => 'Buscar CFDI',
-            'ctl00$MainContent$CldFechaInicial2$Calendario_text' => $startDay . '/' . $startMonth . '/' . $startYear,
-            'ctl00$MainContent$CldFechaInicial2$DdlHora' => $this->query->getStartDate()->format('H'),
-            'ctl00$MainContent$CldFechaInicial2$DdlMinuto' => $this->query->getStartDate()->format('i'),
-            'ctl00$MainContent$CldFechaInicial2$DdlSegundo' => $this->query->getStartDate()->format('s'),
-            'ctl00$MainContent$CldFechaFinal2$Calendario_text' => $startDay . '/' . $startMonth . '/' . $startYear,
-            'ctl00$MainContent$CldFechaFinal2$DdlHora' => $this->query->getEndDate()->format('H'),
-            'ctl00$MainContent$CldFechaFinal2$DdlMinuto' => $this->query->getEndDate()->format('i'),
-            'ctl00$MainContent$CldFechaFinal2$DdlSegundo' => $this->query->getEndDate()->format('s'),
+            'ctl00$MainContent$hfInicial' => $this->query->getStartDate()->format('Y'),
+            'ctl00$MainContent$CldFechaInicial2$Calendario_text' => $this->query->getStartDate()->format('d/m/Y'),
+            'ctl00$MainContent$CldFechaInicial2$DdlHora' => $this->sidate($this->query->getStartDate(), 'H', 1),
+            'ctl00$MainContent$CldFechaInicial2$DdlMinuto' => $this->sidate($this->query->getStartDate(), 'i', 1),
+            'ctl00$MainContent$CldFechaInicial2$DdlSegundo' => $this->sidate($this->query->getStartDate(), 's', 1),
+            'ctl00$MainContent$CldFechaFinal2$Calendario_text' => $this->query->getEndDate()->format('d/m/Y'),
+            'ctl00$MainContent$hfFinal' => $this->query->getEndDate()->format('Y'),
+            'ctl00$MainContent$CldFechaFinal2$DdlHora' => $this->sidate($this->query->getEndDate(), 'H', 1),
+            'ctl00$MainContent$CldFechaFinal2$DdlMinuto' => $this->sidate($this->query->getEndDate(), 'i', 1),
+            'ctl00$MainContent$CldFechaFinal2$DdlSegundo' => $this->sidate($this->query->getEndDate(), 's', 1),
             'ctl00$MainContent$DdlEstadoComprobante' => '1',
             'ctl00$MainContent$FiltroCentral' => $this->getCentralFilter(),
             'ctl00$MainContent$TxtRfcReceptor' => '',
@@ -41,6 +39,17 @@ class FiltersIssued extends BaseFilters implements Filters
             'ctl00$MainContent$ddlComplementos' => '-1',
             'ctl00$MainContent$hfInicialBool' => 'false',
             'ctl00$ScriptManager1' => 'ctl00$MainContent$UpnlBusqueda|ctl00$MainContent$BtnBusqueda',
+            'ctl00$MainContent$ddlVigente' => '',
+            'ctl00$MainContent$ddlCancelado' => '',
+            'ctl00$MainContent$hfDatos' => '',
+            'ctl00$MainContent$hfFlag' => '',
+            'ctl00$MainContent$hfAux' => '',
+            'ctl00$MainContent$hfDescarga' => '',
+            'ctl00$MainContent$hfCancelacion' => '',
+            'ctl00$MainContent$hfUrlDescarga' => '',
+            'ctl00$MainContent$hfParametrosMetadata' => '',
+            'ctl00$MainContent$hdnValAccion' => '',
+            'ctl00$MainContent$hfXML' => ''
         ];
     }
 
