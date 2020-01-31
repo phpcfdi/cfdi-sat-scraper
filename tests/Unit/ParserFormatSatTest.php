bc9af01fd26abae2264a481a8ea6632a2293bfa6
Carlos C Soto <eclipxe13@gmail.com>
2020-01-31 00:01:55 -0600

    Create test before refactor

---
+++ tests/Unit/ParserFormatSatTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpCfdi\CfdiSatScraper\Tests\Unit;
+
+use PhpCfdi\CfdiSatScraper\ParserFormatSAT;
+use PhpCfdi\CfdiSatScraper\Tests\TestCase;
+
+final class ParserFormatSatTest extends TestCase
+{
+    public function testWithEmptySource(): void
+    {
+        $parser = new ParserFormatSAT('');
+        $this->assertSame([], $parser->getFormValues());
+    }
+
+    public function testWithExactSource(): void
+    {
+        $data = [
+            '__EVENTTARGET' => 'x-event-target',
+            '__EVENTARGUMENT' => 'x-event-argument',
+            '__LASTFOCUS' => 'x-last-focus',
+            '__VIEWSTATE' => 'x-event-viewstate',
+        ];
+        $source = $this->buildFakeSourceData($data);
+        $parser = new ParserFormatSAT($source);
+        $this->assertSame($data, $parser->getFormValues());
+    }
+
+    public function testWithSourceMissingKeys(): void
+    {
+        $data = [
+            '__EVENTTARGET' => 'x-event-target',
+            '__EVENTARGUMENT' => 'x-event-argument',
+            '__FOO_BAR' => 'foo-bar',
+            '__VIEWSTATE' => 'x-event-viewstate',
+        ];
+        $source = $this->buildFakeSourceData($data);
+        $parser = new ParserFormatSAT($source);
+        unset($data['__FOO_BAR']);
+        $this->assertSame($data, $parser->getFormValues());
+    }
+
+    public function testWithSourceOtherKeys(): void
+    {
+        $data = [
+            '__EVENTTARGET' => 'x-event-target',
+            '__EVENTARGUMENT' => 'x-event-argument',
+            '__VIEWSTATE' => 'x-event-viewstate',
+        ];
+        $source = $this->buildFakeSourceData($data);
+        $parser = new ParserFormatSAT($source);
+        $this->assertSame($data, $parser->getFormValues());
+    }
+
+    public function buildFakeSourceData(array $values): string
+    {
+        return implode('', array_map(function ($fieldName, $fieldValue): string {
+            return '|' . implode('|', [strlen($fieldValue), 'hiddenField', $fieldName, $fieldValue]);
+        }, array_keys($values), $values));
+    }
+}
