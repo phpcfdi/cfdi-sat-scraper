7ebbb7b3264d4b737cd79a886c2613186cd6b71f
Carlos C Soto <eclipxe13@gmail.com>
2020-01-31 00:05:06 -0600

    Remove unnecesary/unused public property $values

--- src/ParserFormatSAT.php
+++ src/ParserFormatSAT.php
@@ -11,8 +11,6 @@ class ParserFormatSAT
 {
     public $source;
 
-    public $values;
-
     public $items;
 
     public $valids;
@@ -27,7 +25,6 @@ class ParserFormatSAT
     public function __construct($source)
     {
         $this->source = $source;
-        $this->values = [];
         $this->items = [];
         $this->valids = ['__EVENTTARGET', '__EVENTARGUMENT', '__LASTFOCUS', '__VIEWSTATE'];
     }
@@ -37,15 +34,15 @@ class ParserFormatSAT
      */
     public function getFormValues()
     {
-        $this->values = explode('|', $this->source);
+        $values = explode('|', $this->source);
 
         $this->sorted = [];
-        foreach (range(0, count($this->values) - 1) as $index) {
-            $item = $this->values[$index];
+        foreach (range(0, count($values) - 1) as $index) {
+            $item = $values[$index];
             if (in_array($item, $this->valids)) {
                 $name = $item;
                 $index += 1;
-                $item = $this->values[$index];
+                $item = $values[$index];
                 $this->items[$name] = $item;
             }
         }
