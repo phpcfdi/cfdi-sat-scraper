1ed29cd94e8addd82e5e6cdfd36675b48c33b50f
Carlos C Soto <eclipxe13@gmail.com>
2020-02-21 12:28:26 -0600

    Test XmlDownloader

---
+++ tests/Fakes/FakeXmlDownloaderPromiseHandler.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpCfdi\CfdiSatScraper\Tests\Fakes;
+
+use PhpCfdi\CfdiSatScraper\Contracts\XmlDownloaderPromiseHandlerInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * This implementation of XmlDownloaderPromiseHandlerInterface set
+ * the first input (odds) as successfully downloaded
+ * and second input (pairs) as rejected.
+ *
+ * @internal IMPORTANT: Use this without concurrency, it is not multi thread safe
+ *
+ */
+final class FakeXmlDownloaderPromiseHandler implements XmlDownloaderPromiseHandlerInterface
+{
+    /** @var string[] */
+    private $downloadedUuids = [];
+
+    /** @var string[] */
+    private $rejectedUuids = [];
+
+    /** @var int */
+    private $counter = 0;
+
+    /**
+     * @param string $uuid
+     * @return null
+     */
+    public function append(string $uuid)
+    {
+        if (0 === ($this->counter++) % 2) {
+            $this->downloadedUuids[] = $uuid;
+        } else {
+            $this->rejectedUuids[] = $uuid;
+        }
+        return null;
+    }
+
+    public function promiseFulfilled(ResponseInterface $response, string $uuid)
+    {
+        return $this->append($uuid);
+    }
+
+    public function promiseRejected($reason, string $uuid)
+    {
+        return $this->append($uuid);
+    }
+
+    public function downloadedUuids(): array
+    {
+        return $this->downloadedUuids;
+    }
+
+    public function rejectedUuids(): array
+    {
+        return $this->rejectedUuids;
+    }
+}
