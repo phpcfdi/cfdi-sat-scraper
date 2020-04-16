61c3f863288f17be54e69e20e989092769faae9f
Carlos C Soto <eclipxe13@gmail.com>
2020-04-15 23:44:12 -0500

    improve code style

--- src/Filters/Options/ComplementsOption.php
+++ src/Filters/Options/ComplementsOption.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace PhpCfdi\CfdiSatScraper\Filters\Options;
 
-use Eclipxe\Enum\Enum;
 use Eclipxe\MicroCatalog\MicroCatalog;
 use PhpCfdi\CfdiSatScraper\Contracts\FilterOption;
+use PhpCfdi\CfdiSatScraper\Exceptions\InvalidArgumentException;
 
 /**
  * This is a common use case enum sample
@@ -100,46 +100,166 @@ use PhpCfdi\CfdiSatScraper\Contracts\FilterOption;
 class ComplementsOption extends MicroCatalog implements FilterOption
 {
     protected const VALUES = [
-        'todos' => ['input' => '-1', 'description' => 'Cualquier complemento'],
-        'sinComplemento' => ['input' => '8', 'description' => 'Sin complemento'],
-        'acreditamientoIeps' => ['input' => '4294967296', 'description' => 'Acreditamiento de IEPS'],
-        'aerolineas' => ['input' => '8388608', 'description' => 'Aerolíneas'],
-        'certificadoDestruccion' => ['input' => '1073741824', 'description' => 'Certificado de destrucción'],
-        'comercioExterior' => ['input' => '17179869184', 'description' => 'Comercio exterior 1.0'],
-        'comercioExterior11' => ['input' => '274877906944', 'description' => 'Comercio exterior 1.1'],
-        'compraVentaDivisas' => ['input' => '4', 'description' => 'Compra venta de divisas'],
-        'consumoCombustibles' => ['input' => '16777216', 'description' => 'Consumo de combustibles 1.0'],
-        'consumoCombustibles11' => ['input' => '8796093022208', 'description' => 'Consumo de combustibles 1.1'],
-        'donatarias' => ['input' => '64', 'description' => 'Donatarias'],
-        'estadoCuentaBancario' => ['input' => '256', 'description' => 'Estado de cuenta bancario'],
-        'estadoCuentaCombustibles12' => ['input' => '4398046511104', 'description' => 'Estado de cuenta combustibles 1.2'],
-        'estadoCuentaCombustiblesMonederoElectronico' => ['input' => '8589934592', 'description' => 'Estado de cuenta combustibles monedero electrónico'],
-        'gastosHidrocarburos' => ['input' => '17592186044416', 'description' => 'Gastos contrato de hidrocarburos'],
-        'ine11' => ['input' => '68719476736', 'description' => 'INE 1.1'],
-        'ingresosHidrocarburos' => ['input' => '35184372088832', 'description' => 'Ingresos contrato de hidrocarburos'],
-        'institucionesEducativasPrivadas' => ['input' => '1024', 'description' => 'Instituciones educativas privadas'],
-        'leyendasFiscales' => ['input' => '4096', 'description' => 'Leyendas fiscales'],
-        'misCuentas' => ['input' => '524288', 'description' => 'Mis cuentas'],
-        'notariosPublicos' => ['input' => '67108864', 'description' => 'Notarios públicos'],
-        'obraArtesAntiguedades' => ['input' => '536870912', 'description' => 'Obras de artes plásticas y antigüedades'],
-        'otrosDerechosImpuestos' => ['input' => '2048', 'description' => 'Otros derechos e impuestos'],
-        'pagoEspecie' => ['input' => '4194304', 'description' => 'Pago en especie'],
-        'personaFisicaIntegranteCoordinado' => ['input' => '8192', 'description' => 'Personas físicas integrantes de coordinados'],
-        'recepcionPagos' => ['input' => '549755813888', 'description' => 'Complemento para recepción de pagos'],
-        'reciboDonativo' => ['input' => '128', 'description' => 'Recibo de donativo'],
-        'reciboPagoSalarios' => ['input' => '1048576', 'description' => 'Nómina 1.1'],
-        'reciboPagoSalarios12' => ['input' => '137438953472', 'description' => 'Nómina 1.2'],
-        'sectorVentasDetalle' => ['input' => '32', 'description' => 'Facturas del sector de ventas al detalle'],
-        'serviciosConstruccion' => ['input' => '268435456', 'description' => 'Servicios parciales de construcción'],
-        'speiTerceroTercero' => ['input' => '16384', 'description' => 'SPEI Tercero a Tercero'],
-        'sustitucionRenovacionVehicular' => ['input' => '2147483648', 'description' => 'Renovación y sustitución vehicular'],
-        'terceros1' => ['input' => '32768', 'description' => 'Concepto por cuenta de terceros 1.1'],
-        'terceros2' => ['input' => '65536', 'description' => 'Concepto por cuenta de terceros 2.0'],
-        'timbreFiscalDigital' => ['input' => '2199023255552', 'description' => 'Timbre Fiscal Digital'],
-        'turistaPasajeroExtranjero' => ['input' => '16', 'description' => 'Turista pasajero extranjero'],
-        'valesDespensa' => ['input' => '33554432', 'description' => 'Vales de despensa'],
-        'vehiculoUsado' => ['input' => '134217728', 'description' => 'Vehiculos usados'],
-        'ventaVehiculos' => ['input' => '2097152', 'description' => 'Venta de vehiculos nuevos'],
+        'todos' => [
+            'input' => '-1',
+            'description' => 'Cualquier complemento',
+        ],
+        'sinComplemento' => [
+            'input' => '8',
+            'description' => 'Sin complemento',
+        ],
+        'acreditamientoIeps' => [
+            'input' => '4294967296',
+            'description' => 'Acreditamiento de IEPS',
+        ],
+        'aerolineas' => [
+            'input' => '8388608',
+            'description' => 'Aerolíneas',
+        ],
+        'certificadoDestruccion' => [
+            'input' => '1073741824',
+            'description' => 'Certificado de destrucción',
+        ],
+        'comercioExterior' => [
+            'input' => '17179869184',
+            'description' => 'Comercio exterior 1.0',
+        ],
+        'comercioExterior11' => [
+            'input' => '274877906944',
+            'description' => 'Comercio exterior 1.1',
+        ],
+        'compraVentaDivisas' => [
+            'input' => '4',
+            'description' => 'Compra venta de divisas',
+        ],
+        'consumoCombustibles' => [
+            'input' => '16777216',
+            'description' => 'Consumo de combustibles 1.0',
+        ],
+        'consumoCombustibles11' => [
+            'input' => '8796093022208',
+            'description' => 'Consumo de combustibles 1.1',
+        ],
+        'donatarias' => [
+            'input' => '64',
+            'description' => 'Donatarias',
+        ],
+        'estadoCuentaBancario' => [
+            'input' => '256',
+            'description' => 'Estado de cuenta bancario',
+        ],
+        'estadoCuentaCombustibles12' => [
+            'input' => '4398046511104',
+            'description' => 'Estado de cuenta combustibles 1.2',
+        ],
+        'estadoCuentaCombustiblesMonederoElectronico' => [
+            'input' => '8589934592',
+            'description' => 'Estado de cuenta combustibles monedero electrónico',
+        ],
+        'gastosHidrocarburos' => [
+            'input' => '17592186044416',
+            'description' => 'Gastos contrato de hidrocarburos',
+        ],
+        'ine11' => [
+            'input' => '68719476736',
+            'description' => 'INE 1.1',
+        ],
+        'ingresosHidrocarburos' => [
+            'input' => '35184372088832',
+            'description' => 'Ingresos contrato de hidrocarburos',
+        ],
+        'institucionesEducativasPrivadas' => [
+            'input' => '1024',
+            'description' => 'Instituciones educativas privadas',
+        ],
+        'leyendasFiscales' => [
+            'input' => '4096',
+            'description' => 'Leyendas fiscales',
+        ],
+        'misCuentas' => [
+            'input' => '524288',
+            'description' => 'Mis cuentas',
+        ],
+        'notariosPublicos' => [
+            'input' => '67108864',
+            'description' => 'Notarios públicos',
+        ],
+        'obraArtesAntiguedades' => [
+            'input' => '536870912',
+            'description' => 'Obras de artes plásticas y antigüedades',
+        ],
+        'otrosDerechosImpuestos' => [
+            'input' => '2048',
+            'description' => 'Otros derechos e impuestos',
+        ],
+        'pagoEspecie' => [
+            'input' => '4194304',
+            'description' => 'Pago en especie',
+        ],
+        'personaFisicaIntegranteCoordinado' => [
+            'input' => '8192',
+            'description' => 'Personas físicas integrantes de coordinados',
+        ],
+        'recepcionPagos' => [
+            'input' => '549755813888',
+            'description' => 'Complemento para recepción de pagos',
+        ],
+        'reciboDonativo' => [
+            'input' => '128',
+            'description' => 'Recibo de donativo',
+        ],
+        'reciboPagoSalarios' => [
+            'input' => '1048576',
+            'description' => 'Nómina 1.1',
+        ],
+        'reciboPagoSalarios12' => [
+            'input' => '137438953472',
+            'description' => 'Nómina 1.2',
+        ],
+        'sectorVentasDetalle' => [
+            'input' => '32',
+            'description' => 'Facturas del sector de ventas al detalle',
+        ],
+        'serviciosConstruccion' => [
+            'input' => '268435456',
+            'description' => 'Servicios parciales de construcción',
+        ],
+        'speiTerceroTercero' => [
+            'input' => '16384',
+            'description' => 'SPEI Tercero a Tercero',
+        ],
+        'sustitucionRenovacionVehicular' => [
+            'input' => '2147483648',
+            'description' => 'Renovación y sustitución vehicular',
+        ],
+        'terceros1' => [
+            'input' => '32768',
+            'description' => 'Concepto por cuenta de terceros 1.1',
+        ],
+        'terceros2' => [
+            'input' => '65536',
+            'description' => 'Concepto por cuenta de terceros 2.0',
+        ],
+        'timbreFiscalDigital' => [
+            'input' => '2199023255552',
+            'description' => 'Timbre Fiscal Digital',
+        ],
+        'turistaPasajeroExtranjero' => [
+            'input' => '16',
+            'description' => 'Turista pasajero extranjero',
+        ],
+        'valesDespensa' => [
+            'input' => '33554432',
+            'description' => 'Vales de despensa',
+        ],
+        'vehiculoUsado' => [
+            'input' => '134217728',
+            'description' => 'Vehiculos usados',
+        ],
+        'ventaVehiculos' => [
+            'input' => '2097152',
+            'description' => 'Venta de vehiculos nuevos',
+        ],
     ];
 
     /**
