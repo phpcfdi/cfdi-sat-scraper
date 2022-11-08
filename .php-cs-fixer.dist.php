63b86b5bf610186308e205c060f07dc52e8a0dd1
Carlos C Soto <eclipxe13@gmail.com>
2022-11-08 13:21:54 -0600

    Fix deprecated no_trailing_comma_in_singleline_array

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -28,7 +28,7 @@ return (new PhpCsFixer\Config())
         'object_operator_without_whitespace' => true,
         'binary_operator_spaces' => true,
         'phpdoc_scalar' => true,
-        'no_trailing_comma_in_singleline_array' => true,
+        'no_trailing_comma_in_singleline' => true,
         'single_quote' => true,
         'no_singleline_whitespace_before_semicolons' => true,
         'no_unused_imports' => true,
